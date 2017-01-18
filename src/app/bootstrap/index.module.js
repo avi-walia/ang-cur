65b77b203732abc389b7284948cdd6485c491082
Vanessa Villanueva <vvillanueva@ci.com>
2017-01-18 18:33:54 -0500

    added some wizard in index.module

--- src/app/bootstrap/index.module.js
+++ src/app/bootstrap/index.module.js
@@ -6,6 +6,22 @@
     angular.module('evolution.core.server', ['evolution.core.cache']);
     angular.module('evolution.core.main', ['evolution.core.server']);
     //---------------- FEATURES ---------------- //
+    angular.module('evolution.features.selectClientProfile', ['evolution.utils']);
+    angular.module('evolution.features.clientAccountInfo', ['evolution.utils']);
+    angular.module('evolution.features.ipq', ['evolution.utils']);
+    angular.module('evolution.features.portfolioSelection', ['evolution.utils']);
+    angular.module('evolution.features.fundCustomization', ['evolution.utils']);
+    angular.module('evolution.features.advisorInfo', ['evolution.utils']);
+    angular.module('evolution.features.profileSummary', ['evolution.utils']);
+    angular.module('evolution.features.reports', ['evolution.utils']);
+    angular.module('evolution.features.splitProfile', ['evolution.utils']);
+    angular.module('evolution.features.help', ['evolution.utils']);
+    angular.module('evolution.features.fee.profileSearch', ['evolution.utils']);
+    angular.module('evolution.features.fee.contactInfo', ['evolution.utils']);
+    angular.module('evolution.features.fee.investmentOverview', ['evolution.utils']);
+    angular.module('evolution.features.fee.serviceFees', ['evolution.utils']);
+    angular.module('evolution.features.fee.summary', ['evolution.utils']);
+    angular.module('evolution.features.fee.reports', ['evolution.utils']);
     angular.module('evolution.features.searchByName', ['evolution.utils']);
     angular.module('evolution.features.test', ['evolution.utils']);
     angular.module('evolution.features.test2', ['evolution.utils']);
@@ -27,6 +43,22 @@
         'ngSanitize',
         'ngAria',
         'evolution.core.main',
+        'evolution.features.selectClientProfile',
+        'evolution.features.clientAccountInfo',
+        'evolution.features.ipq',
+        'evolution.features.portfolioSelection',
+        'evolution.features.fundCustomization',
+        'evolution.features.advisorInfo',
+        'evolution.features.profileSummary',
+        'evolution.features.reports',
+        'evolution.features.splitProfile',
+        'evolution.features.help',
+        'evolution.features.fee.profileSearch',
+        'evolution.features.fee.contactInfo',
+        'evolution.features.fee.investmentOverview',
+        'evolution.features.fee.serviceFees',
+        'evolution.features.fee.summary',
+        'evolution.features.fee.reports',
         'evolution.features.searchByName',
         'evolution.features.test',
         'evolution.features.test2',
