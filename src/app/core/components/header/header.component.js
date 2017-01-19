fe7d8cb4033b6e584d70a66a770caabb099bd9e9
Vanessa Villanueva <vvillanueva@ci.com>
2017-01-19 18:36:40 -0500

    added eciss , profile name & open/create profile links in header tpl

--- src/app/core/components/header/header.component.js
+++ src/app/core/components/header/header.component.js
@@ -19,6 +19,8 @@
     function headerCtrl($state, pageStateResolver) {
         var vm = this;
         vm.isActive = true;
+        vm.displayOpenExistingPro = false; //flag used if we want to display Create/open profile link in navigation
+        vm.isEcissUser = false;
         vm.pageStateResolver = pageStateResolver;
 
         // var tmpArr = $state.current.name.split('.');
@@ -26,8 +28,18 @@
         // console.log(tmp[tmp.length-1]);
         vm.state = tmp[tmp.length-1];
 
+        if (vm.state !== 'selectClientProfile'){
+            vm.displayOpenExistingPro = true;
+        }
+
+        //need to find a way to determine if user is eciss
+        //assuming it is until we get API call
+        vm.isEcissUser = true;
+
+        vm.profileName = "Best profile ever!";
+
         // console.log($state.current.name);
-        // // console.log(vm.stepIndicator);
+        // console.log(vm.pageStateResolver.stepIndicator);
         // console.log(vm.pageStateResolver.stepIndicator);
         //
         //
