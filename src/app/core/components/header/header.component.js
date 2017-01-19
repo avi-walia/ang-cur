96aa8952df6ed63bfdd6db7c36187fef8f9abbf5
Vanessa Villanueva <vvillanueva@ci.com>
2017-01-19 17:51:23 -0500

    added step indicator functionality in header component

--- src/app/core/components/header/header.component.js
+++ src/app/core/components/header/header.component.js
@@ -6,18 +6,34 @@
         .module('evolution.core.main')
         .component('ciHeader', {
             controller: headerCtrl,
-            templateUrl:'app/core/components/header/header.tpl.html'
+            templateUrl:'app/core/components/header/header.tpl.html',
+            bindings: {
+                stepIndicator: '<?'
+            }
         });
 
     /* @ngInject */
 
-    headerCtrl.$inject = ['$state'];
+    headerCtrl.$inject = ['$state', 'pageStateResolver'];
     /* @ngInject */
-    function headerCtrl($state) {
+    function headerCtrl($state, pageStateResolver) {
         var vm = this;
         vm.isActive = true;
+        vm.pageStateResolver = pageStateResolver;
 
-        console.log($state.current.name);
+        // var tmpArr = $state.current.name.split('.');
+        var tmp = _.split($state.current.name, '.');
+        // console.log(tmp[tmp.length-1]);
+        vm.state = tmp[tmp.length-1];
+
+        // console.log($state.current.name);
+        // // console.log(vm.stepIndicator);
+        // console.log(vm.pageStateResolver.stepIndicator);
+        //
+        //
+        // console.log(vm.pageStateResolver.activePageName);
+
+
     }
 
 })();
