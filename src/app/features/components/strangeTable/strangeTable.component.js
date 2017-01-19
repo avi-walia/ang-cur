c6e8f95468695621ee83245d7729eed44a3133ce
Chong, Michael <michong@ci.com>
2017-01-19 17:03:26 -0500

    Added dynamic table with sorting.
    API dataset is not in the right format to work with the sorting in the table. Need to revisit API design.

--- src/app/features/components/strangeTable/strangeTable.component.js
+++ src/app/features/components/strangeTable/strangeTable.component.js
@@ -24,10 +24,47 @@
         $timeout
     ) {
         var vm = this;
+        vm.sortable = {};
+        vm.changeOrder = changeOrder;
+        vm.orderBy = 'id';
+        vm.isAscending = true;
+
+
         $timeout(function(){
 
             console.log('data: ', vm.data);
         },1000);
+
+        /*
+        _.forEach(vm.data[0], function(val, key) {
+
+
+        });
+        */
+
+        function compare(a,b) {
+            var x = 1;
+            if (vm.isAscending) {
+                x = -1;
+            }
+            if (a[vm.orderBy] < b[vm.orderBy])
+                return -1 * x;
+            if (a[vm.orderBy] > b[vm.orderBy])
+                return x;
+            return 0;
+        }
+
+        function changeOrder(order) {
+            console.log('new order: ', order);
+            if (vm.orderBy !== order) {
+                vm.isAscending = !vm.isAscending
+            }
+            vm.orderBy = order;
+            vm.data.sort(compare);
+
+            console.log('sorted data: ', vm.data);
+
+        }
     }
 
 })();
