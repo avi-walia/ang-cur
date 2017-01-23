3720454aac01ff3c50542a60dd238706af75eea1
Vanessa Villanueva <vvillanueva@ci.com>
2017-01-23 10:09:11 -0500

    Modal component added

---
+++ src/app/utils/components/modal/modal.component.js
@@ -0,0 +1,50 @@
+
+(function () {
+    'use strict';
+
+    angular
+        .module('evolution.utils')
+        .component('modalComponent', {
+            controller: modalCtrl,
+            bindings: {
+                resolve: '<',
+                close: '&',
+                dismiss: '&'
+            },
+        templateUrl:'app/utils/components/modal/modal.tpl.html'
+        });
+
+
+    /* @ngInject */
+//    modalCtrl.$inject = ['$uibModalInstance'];
+  
+    /* @ngInject */
+    function modalCtrl() {
+        var vm = this;
+        vm.ok = ok
+        vm.cancel = cancel;
+        vm.msg = '';
+          
+        vm.$onInit = function () {
+
+            vm.msg = vm.resolve.dynamicMsg;
+
+        };
+
+        function ok() {
+           vm.close();
+            
+        };
+
+        function cancel() {
+            vm.dismiss();
+        };
+        
+    }
+
+})();
+
+
+
+
+
