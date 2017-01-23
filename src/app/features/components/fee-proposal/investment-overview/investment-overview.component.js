25c49491f14c270145a8f5de4ad4e5e19fc7dd27
Vanessa Villanueva <vvillanueva@ci.com>
2017-01-23 14:38:19 -0500

    start of Inv Overview

--- src/app/features/components/fee-proposal/investment-overview/investment-overview.component.js
+++ src/app/features/components/fee-proposal/investment-overview/investment-overview.component.js
@@ -7,15 +7,25 @@
             controller: investmentOverviewCtrl,
             bindings: {},
             templateUrl: 'app/features/components/fee-proposal/investment-overview/investment-overview.tpl.html'
-
         });
 
-    investmentOverviewCtrl.$inject = ['$scope'];
+    investmentOverviewCtrl.$inject = ['mockService', 'waitForResourcesService'];
 
     /* @ngInject */
-    function investmentOverviewCtrl($scope) {
+    function investmentOverviewCtrl(mockService, waitForResourcesService) {
         var vm = this;
-        this.functionName = functionName;
+        vm.totalGroupAmount = 932523523;//this is the sum of all IP group's investment amounts & LInked CI Assets
+
+        vm.mockService = mockService;
+
+        // console.log(vm.mockService.data[0][0][0]);
+        var tmpFamilyGroup = vm.mockService.data[0][0][0];
+
+        vm.profiles = tmpFamilyGroup.profileSummaries;
+
+        // console.log(profiles); //for now this is length =7 but this should be dynamic!
+
+        vm.functionName = functionName;
 
         ////////////////
 
