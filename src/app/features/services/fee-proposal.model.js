5099118140b8ea8f322b0800a79cc200e954489c
Vanessa Villanueva <vvillanueva@ci.com>
2017-01-23 11:07:10 -0500

    Model to use to keep track of fee proposal changes. Not finalized

---
+++ src/app/features/services/fee-proposal.model.js
@@ -0,0 +1,23 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('evolution.core.main')
+        .service('feeProposalModel', feeProposalModel);
+
+//    profileSearchModel.$inject = [
+//        'languageSwitcherService',
+//        'server',
+//        'waitForResourcesService'
+//    ];
+
+    /* @ngInject */
+    function feeProposalModel() {
+        var service = this;
+        console.log('feeProposalModel');
+//        service.dealerRepCode = 2; //default value
+        service.familyGroupName = 'John & James';
+        
+    }
+
+})();
