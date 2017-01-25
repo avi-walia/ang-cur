77d1a0916e3ad457a1173113c954c3a6742d26b4
Chong, Michael <michong@ci.com>
2017-01-25 10:23:03 -0500

    Created a feeProposalService to store the whole data model for every page of the fee proposal tool. Each page of the fee proposal tool should have it’s own service that has feeProposalService as a dependency. Each page’s service should interact with feeProposalService through getters and setters such as getInvestmentOverview and setInvestmentOverview, when on the investmentOverview screen.

--- src/app/features/services/fee-proposal.service.js
+++ src/app/features/services/fee-proposal.service.js
@@ -12,54 +12,148 @@
     function feeProposalService() {
         var service = this;
         service.data = {
-            profiles: [],
-            dealerRepCode: "",
+            /*
+                partialProfiles is an array of partialProfileObjects
+                partialProfile only contains the following:
+                profileId
+                assetMix & funds
+                investmentOverview object
+                array of dealerServiceFee objects
+                example of a partial profile
+             */
+            partialProfiles: [],
             familyGroupName: "",
-            contactInfo: {
-                //advisor info
-                fullName:"",
-                Email: "",
-                phone: "",
-                fax: "",
+            //contact info
+            firstName:"",
+            middleName:"",
+            lastName:"",
+            address: "",
+            city: "",
+            province: "",//required
+            postalCode:"",
+            phone: "",
+            fax: "",
+            Email: "",
 
-                //mailing address
-                street: "",
-                province: "",//required
-                city: "",
-                postalCode:""
-            },
-            investmentOverview: {
-                linkedCIAssets: 0,
-                totalFamilyGroupAmount: 0,
-                profileInvestments: []//can hold one object if launched from fund customization screen, otherwise will have atleast 2 entries
-            }
+            //investment overview
+            linkedCIAssets: 0,
+            totalFamilyGroupAmount: 0
         };
 
-        function profileInvestments() {
+        function getInvestmentOverview() {
+            var subsetPartialProfile = [];
+            _.forEach(service.data.partialProfiles, function(partialProfile) {
+                subsetPartialProfile.push({
+                    profileId: partialProfile.profileId,//immutable from investmentOverview screen
+                    profileName: partialProfile.profileName,
+                    assetMix: partialProfile.assetMix,//immutable from investmentOverview screen
+                    managedMarketValue: partialProfile.managedMarketValue,
+                    totalIPGroupInvestmentAmount: partialProfile.totalIPGroupInvestmentAmount,
+                    separatelyManagedAccounts: partialProfile.separatelyManagedAccounts
+                });
+            });
+
             return {
-                investmentAmount: {
-                    classI: 0,
-                    classE: 0,
-                    classF: 0,
-                    totalIPGroupInvestmentAmount: 0
-                },
-                separatelyManagedAccounts: []
+                familyGroupName: service.data.familyGroupName,//immutable from investmentOverview screen
+                linkedCIAssets: service.data.linkedCIAssets,
+                totalFamilyGroupAmount: service.data.totalFamilyGroupAmount,
+                partialProfiles: subsetPartialProfile
+            };
+        }
+
+        function setInvestmentOverview(newInvestmentOverview) {
+            //add some data validation here?
+
+            service.data.linkedCIAssets = newInvestmentOverview.linkedCIAssets;
+            service.data.totalFamilyGroupAmount = newInvestmentOverview.totalFamilyGroupAmount;
+
+            _.forEach(newInvestmentOverview.partialProfiles, function(partialProfile, key) {
+                //reset the managedMarketValues to 0
+                service.data.partialProfiles[key].separatelyManagedMarketValue = valueByClass();
+                service.data.partialProfiles[key].profileName = partialProfile.profileName,
+                    service.data.partialProfiles[key].managedMarketValue = partialProfile.managedKarketValue;
+                    service.data.partialProfiles[key].totalIPGroupInvestmentAmount = partialProfile.totalIPGroupInvestmentAmount,
+                    service.data.partialProfiles[key].separatelyManagedAccounts = partialProfile.separatelyManagedAccounts;
+                _.forEach(partialProfile.separatelyManagedAccounts, function(sepManAcc, key) {
+                    //updated the managedMarketValues
+                    //need to be done this way because there could be two different separately managed accounts with the same fundClass.
+                    service.data.partialProfiles[key].separatelyManagedMarketValue[sepManAcc.fundClass] += sepManAcc.allocation;
+                });
+            });
+        }
+
+        function getServiceFees() {
+            var tempServiceFees = [];
+            _.forEach(service.data.partialProfiles, function(partialProfile) {
+                var managed = [];
+                var separatelyManaged = [];
+                _.forEach(partialProfile.managedMarketValue, function(marketValue, fundClass) {
+                    managed.push({
+                        class: fundClass,
+                        marketValue: marketValue,
+                        serviceFee: partialProfile.managedServiceFees[fundClass]
+                    });
+                });
+                _.forEach(partialProfile.separatelyManagedMarketValue, function(marketValue, fundClass) {
+                    separatelyManaged.push({
+                        class: fundClass,
+                        marketValue: marketValue,
+                        serviceFee: partialProfile.separatelyManagedServiceFees[fundClass]
+                    });
+                });
+                tempServiceFees.push({
+                    profileId: partialProfile.profileId,
+                    profileName: partialProfile.profileName,
+                    managed: managed,
+                    separatelyManaged: separatelyManaged
+                });
+            });
+            return tempServiceFees;
+        }
+
+        /*
+        partialProfiles is an array of partialProfileObjects
+        partialProfile only contains the following:
+            profileId
+        assetMix & funds
+        investmentOverview object
+        array of dealerServiceFee objects
+        example of a partial profile
+        */
+        function partialProfile(loadedProfile) {
+            return {
+                profileId: loadedProfile.profileId,
+                profileName: loadedProfile.profileName,
+                assetMix: loadedProfile.assetMix,
+                managedMarketValue: valueByClass(),
+                separatelyManagedMarketValue: valueByClass(),
+                totalIPGroupInvestmentAmount: 0,
+                managedServiceFees: serviceFees(),
+                separatelyManagedAccounts: [],
+                separatelyManagedServiceFees: valueByClass()
             }
         }
-        function separatelyManagedAccounts() {
+
+        function separatelyManagedAccount() {
             return {
                 accountType: "",
-                fundClass: "",
+                fundClass: "",//should be one of the string keys in the valueByClass object, ie. "classI", "classE", "classF", and possibly more in the future
                 amount: 0,
-                separatelyManagedFunds: [],
+                separatelyManagedFunds: []
             }
         }
-        function separatelyManagedFunds() {
+        function separatelyManagedFund() {
             return {
                 fundName: "",
-                fundFamily: "",
-                currency: "",
-                
+                assetClass: "",
+                allocation:0
+            }
+        }
+        function valueByClass() {
+            return {
+                classI: 0,
+                classE: 0,//class E is actually called a trailer fee.
+                classF: 0
             }
         }
     }
