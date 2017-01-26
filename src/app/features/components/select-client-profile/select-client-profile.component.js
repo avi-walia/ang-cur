a0d7fb6eeb75dde6663c73f68b790d52f00f951f
Vanessa Villanueva <vvillanueva@ci.com>
2017-01-26 12:22:43 -0500

    added Select Client Profile service. Added call to get all funds that will be stored in local storage

--- src/app/features/components/select-client-profile/select-client-profile.component.js
+++ src/app/features/components/select-client-profile/select-client-profile.component.js
@@ -5,10 +5,35 @@
         bindings: {},
         templateUrl: 'app/features/components/select-client-profile/select-client-profile.tpl.html'
     });
-    selectClientProfileCtrl.$inject = ['$scope'];
+    selectClientProfileCtrl.$inject = ['$state', '$window', 'dataCacheLocalStorage', 'selectClientProfileService'];
     /* @ngInject */
-    function selectClientProfileCtrl($scope) {
+    function selectClientProfileCtrl($state, $window, dataCacheLocalStorage, selectClientProfileService) {
         var vm = this;
+        vm.goToFeeProposal = goToFeeProposal;
+
+        function goToFeeProposal() {
+
+            //lets grab all funds
+            getFunds().then(function (data) {
+                console.log(data);
+                dataCacheLocalStorage.put('fundList', data);
+            });
+            var url = $state.href('main.evolution.fee.profileSearch');
+            $window.open(url, '_blank');
+
+        }
+
+        function getFunds() {
+
+            return selectClientProfileService.getData('/getAllFundList')
+                .then(function (data) {
+                    return data;
+                }, function (error) {
+                    console.error('Error: selectClientProfileService getData call');
+                    return;
+                });
+
+        }
 
 
     }
