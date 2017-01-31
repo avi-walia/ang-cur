8547677747203957cf6d9454c414bc6e0ee7f19e
Vanessa Villanueva <vvillanueva@ci.com>
2017-01-31 16:09:03 -0500

    changed the status to what API returns and added some comments

--- src/app/features/services/fee-proposal/profile-search.service.js
+++ src/app/features/services/fee-proposal/profile-search.service.js
@@ -14,13 +14,11 @@
     ];
 
     /* @ngInject */
-    function profileSearchService(
-        feeProposalService,
-        languageSwitcherService,
-        server,
-        $state,
-        waitForResourcesService
-    ) {
+    function profileSearchService(feeProposalService,
+                                  languageSwitcherService,
+                                  server,
+                                  $state,
+                                  waitForResourcesService) {
         var service = this;
         service.showValidationErrors = false;
         service.data = [];
@@ -74,21 +72,21 @@
             init();
             service.resetFlag = !service.resetFlag;
         }
-		
-		/*
-		make sure all the required fields are filled out in service.familyGroupInfo
-		return true if all fields are filled in.
-		return false otherwise.
-		*/
-		function validate(form) {
-		    var enoughProfiles = service.familyGroupInfo.partialProfiles.length > 1;
+
+        /*
+         make sure all the required fields are filled out in service.familyGroupInfo
+         return true if all fields are filled in.
+         return false otherwise.
+         */
+        function validate(form) {
+            var enoughProfiles = service.familyGroupInfo.partialProfiles.length > 1;
             if (!enoughProfiles) {
                 service.errors = ['profilesInFamilyGroupErrors.tooFew'];
             } else {
                 service.errors = [];
             }
-			return (form.$valid && _.isArray(service.familyGroupInfo.partialProfiles) && enoughProfiles);
-		}
+            return (form.$valid && _.isArray(service.familyGroupInfo.partialProfiles) && enoughProfiles);
+        }
 
         function next(nextState, form) {
             if (validate(form)) {
@@ -104,12 +102,15 @@
             service.familyGroupInfo.currentList = currentList;
         }
 
-
+        /*
+         * Called when the user has picked a dealer/rep code
+         *
+         */
         function updateProfileList(dealerRepCode) {
             service.familyGroupInfo.dealerRepCode = dealerRepCode;
 
             console.log('/profile_groups/' + dealerRepCode);
-            var ret = server.getNoStorage('/profile_groups/' + dealerRepCode, true).then(function(data){
+            var ret = server.getNoStorage('/profile_groups/' + dealerRepCode, true).then(function (data) {
                 format(data.data);
                 service.data = data.data;
                 //languageSwitcherService.addLocalizationObject({unLocalized: data.unLocalizedData, callBack: translateData, key: 'profileSearchService_data'})
@@ -119,15 +120,24 @@
             waitForResourcesService.startWaiting();
             return ret;
         }
+
         function format(profileGroups) {
-            _.forEach(profileGroups, function(profileGroup) {
+            _.forEach(profileGroups, function (profileGroup) {
                 //profileGroup.subGroups = profileGroup.profileSummaries;
                 //delete profileGroup.profileSummaries;
-                _.forEach(profileGroup.profileSummaries, function(profileSummary) {
+
+                /* Lets loop through the profileSummaries and clean up the createdBy attribute */
+                _.forEach(profileGroup.profileSummaries, function (profileSummary) {
                     profileSummary.createdBy = stripPrefix(profileSummary.createdBy);
                 });
             });
         }
+
+        /**
+         * Used to clean up the createdBy attribute in Profile groups
+         * @param str
+         * @returns {*}
+         */
         function stripPrefix(str) {
             var prefix = 'UPMA_ADV::';
             if (str.substring(0, prefix.length) === prefix) {
@@ -135,37 +145,54 @@
             }
             return str;
         }
-        /*
-        function translateData(translatedData) {
-            //service.data = translatedData;
-            format(translatedData);
-            _.assign(service.data, translatedData);
-        }*/
+
+        /**
+         * Sets the hierarchy of the profiles based on its status.
+         * Active is the highest whereas cancelled is the lowest
+         * @param status
+         * @returns {number}
+         */
         function mapStatus(status) {
+            //convert status to lowercase since API returns them all in uppercase
+            status = _.toLower(status);
             if (status === 'active') {
                 return 5;
             } else if (status === 'submitted') {
                 return 4;
-            } else if (status === 'inProgress') {
+            } else if (status === 'in_progress') {
                 return 3;
-            } else if (status === 'inActive') {
+            } else if (status === 'inactive') {
                 return 2;
             } else if (status === 'archived') {
                 return 1;
-            } else {//cancelled
+            } else {//status === cancelled
                 return 0;
             }
         }
+
+        /**
+         * Organizes the profiles groups and deciding which ones is the header by comparing its statuses.
+         * Please note profile groups can only have 1 active or submitted statuses
+         * @param profileGroup an object which has its children.
+         */
         function getGroupHeader(profileGroup) {
             var tempProfileIndexWithHighestStatus = 0;
-            _.forEach(profileGroup.profileSummaries, function(profile, key) {
-                if(key > 0) {
-                    if (mapStatus(profile.status) > mapStatus(profileGroup.profileSummaries[tempProfileIndexWithHighestStatus].status)) {
+            var tmpMapStatus, tmpProfileGrpStatus;
+
+            _.forEach(profileGroup.profileSummaries, function (profile, key) {
+                if (key > 0) {
+                    tmpMapStatus = mapStatus(profile.status);
+                    tmpProfileGrpStatus = mapStatus(profileGroup.profileSummaries[tempProfileIndexWithHighestStatus].status);
+
+                    if (tmpMapStatus > tmpProfileGrpStatus) {
                         tempProfileIndexWithHighestStatus = key;
                     }
                 }
             });
+            //main profile header
             profileGroup.groupHeading = profileGroup.profileSummaries[tempProfileIndexWithHighestStatus];
+
+            //the children of profile header
             profileGroup.profileSummaries.splice(tempProfileIndexWithHighestStatus, 1);
         }
     }
